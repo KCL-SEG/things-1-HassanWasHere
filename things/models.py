d526064834e768b576a539571adeb09e2e5b249c
Hassan Mahmood <hassanmahmood@live.co.uk>
2023-12-06 19:00:49 +0000

    Fix quantity value

--- things/models.py
+++ things/models.py
@@ -4,4 +4,4 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Thing(models.Model):
     name = models.CharField(blank=False, max_length=30,unique=True)
     description = models.CharField(max_length=120, blank=True)
-    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(120)])
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
